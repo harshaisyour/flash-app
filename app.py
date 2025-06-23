68e25dbdfe15c9e6a93fd9b147672a2313c52a9e
harshaisyour <141353245+harshaisyour@users.noreply.github.com>
2025-06-23 16:39:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Render!"
+def home():
+    return "Flask app running!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
